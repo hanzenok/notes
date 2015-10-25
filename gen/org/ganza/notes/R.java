05ea79485dc7d5c2dfc841909ef4cebfdcf81f78
Mykhailo Ganza <hanzenok@gmail.com>
2015-10-25 22:33:09 +0100

    Added some strings

--- gen/org/ganza/notes/R.java
+++ gen/org/ganza/notes/R.java
@@ -25,6 +25,8 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f050000;
+        public static final int button_label=0x7f050003;
+        public static final int hint=0x7f050002;
         public static final int note=0x7f050001;
     }
     public static final class style {
